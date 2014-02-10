d417fbbf5bf43e771357d8d66ecab3dfc42a9b9b
José Rose <jrosesol@gmail.com>
2014-02-10 15:40:20 -0500

    Round the time to multiple of 6 minutes
    0.1h -> 6 mins
    0.2h -> 12 mins
    0.3h -> 18 mins
    ....
    1h -> 60 mins

--- timesheet.js
+++ timesheet.js
@@ -64,7 +64,10 @@ function eventCallback(response) {
             
             var startTime = new Date(e.startTime.year, e.startTime.month, e.startTime.date, e.startTime.hour, e.startTime.minute, e.startTime.second);
             var endTime = new Date(e.endTime.year, e.endTime.month, e.endTime.date, e.endTime.hour, e.endTime.minute, e.endTime.second)
-            calendarEventsCount += Math.abs(endTime.getTime() - startTime.getTime())/1000/60/60;
+            var eventDuration = Math.abs(endTime.getTime() - startTime.getTime())/1000/60/60;
+            // Round to 6 minutes
+            eventDuration = Math.floor(((eventDuration - Math.floor(eventDuration))*6)/0.1)/100
+            calendarEventsCount += eventDuration;
             //calendarEventsCount += ((e.endTime.hour - e.startTime.hour) + ((e.endTime.minute - e.startTime.minute)/60.0));
         }
       }
